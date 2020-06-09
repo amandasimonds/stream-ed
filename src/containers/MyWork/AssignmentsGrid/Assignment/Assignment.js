ffcbd320489cbf46b8ab7074076b861a54d6cebd
Amanda Tree <amandasimonds9@gmail.com>
2020-06-09 15:46:19 -0600

    trying to loop through assignment data to display the whol array on the page, but its not looping, this is the closest I've gotten so far

--- src/containers/MyWork/AssignmentsGrid/Assignment/Assignment.js
+++ src/containers/MyWork/AssignmentsGrid/Assignment/Assignment.js
@@ -4,7 +4,7 @@ import classes from "./Assignment.css"
 const Assignment = (props) => (
 
     <div className={classes.AssignmentRow}>
-            Title: {props.title}
+            Title: {props.title} | Subject: {props.subject}
         </div>
     );
 
--- src/containers/MyWork/AssignmentsGrid/AssignmentsGrid.js
+++ src/containers/MyWork/AssignmentsGrid/AssignmentsGrid.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 import axios from "../../../axios-assignments"
 
 import classes from "./AssignmentsGrid.css"
@@ -6,45 +6,54 @@ import classes from "./AssignmentsGrid.css"
 import FilterItem from "../../../components/StreamContent/Filters/FilterItem/FilterItem";
 import Assignment from "./Assignment/Assignment"
 
-class AssignmentsGrid extends Component{
+class AssignmentsGrid extends Component {
 
-    state={
-        assignments:[]
+    state = {
+        assignments: []
     }
 
-    componentDidMount(){
+    componentDidMount() {
         axios.get("/assignments.json")
-            .then(response =>{
-                console.log(response)
+            .then(response => {
                 let assignments = response.data;
-                
                 let updatedAssignments = Object.values(assignments)
-                
-                this.setState({assignments:updatedAssignments})
-                console.log(this.state.assignments)
+                this.setState({ assignments: updatedAssignments })
             })
-    }
+    };
 
-    render(){
-        return(
-    <div className={classes.AssignmentsGrid}>
-        <div className={classes.firstRow}>
-                <h1>{this.props.subjectName}</h1>
-                <FilterItem filterType="Classwork">classwork</FilterItem>
-                <FilterItem filterType="Tests">Tests</FilterItem>
-                <FilterItem filterType="Homework">Homework</FilterItem>
-        </div> 
-        {this.state.assignments.map(assignment => (
-            <Assignment 
-                title={this.state.assignments.title}
-                body={this.state.assignments.body}
-                subject={this.state.assignments.subject}/>
-        ))} 
+    render() {
 
-        
+        let assignmentsBox = null
+
+        return (
+            <div className={classes.AssignmentsGrid}>
+                <div className={classes.firstRow}>
+                    <h1>{this.props.subjectName}</h1>
+                    <FilterItem filterType="Classwork">classwork</FilterItem>
+                    <FilterItem filterType="Tests">Tests</FilterItem>
+                    <FilterItem filterType="Homework">Homework</FilterItem>
+                </div>
+
+                {this.state.assignments.map((assignmentsBox, key) => {
+                    // let assignmentsBox
+                    for (let i = 0; i < this.state.assignments.length; i++) {
+                        console.log(this.state.assignments)
+                        console.log("assignments loop", this.state.assignments[i].title)
+                        assignmentsBox = (
+                            <div key={key}>
+                                <Assignment
+                                    title={this.state.assignments[i].title}
+                                    body={this.state.assignments[i].body}
+                                    subject={this.state.assignments[i].subject} />
+                            </div>
+                        )}
+                    return assignmentsBox
+                })}
+                
+                {assignmentsBox}
             </div>
-)
-        }
+        )
     }
+}
 
 export default AssignmentsGrid;
