e7b1de06d14beffd8b538dbfec3b77380dc9db9d
Amanda Tree <amandasimonds9@gmail.com>
2020-06-19 14:07:45 -0600

    added hashrouter

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 // import Layout from "./components/layout/Layout";
 import Stream from "../containers/Stream";
