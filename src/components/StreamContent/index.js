dac6c8cea82d75f5a6ddac795e288bc0d0937dc2
Richard Germaine <rlgerma92@outlook.com>
2020-07-09 18:35:53 -0600

    styling tweak. Dash is now centered and mobile responsive

--- src/components/StreamContent/index.js
+++ src/components/StreamContent/index.js
@@ -9,12 +9,35 @@ import StreamPost from "./StreamPosts/StreamPost";
 class StreamContent extends Component {
   state = {
     filters: [
-      { type: "LiveStream", text: "live streams" },
-      { type: "Announcement", text: "announcements" },
-      { type: "Teacher", text: "teacher" },
-      { type: "Subject", text: "social studies" },
-      { type: "Subject", text: "math" },
-      { type: "Subject", text: "language arts" },
+      {
+        type: "LiveStream",
+        text: "live streams",
+      },
+
+      {
+        type: "Announcement",
+        text: "announcements",
+      },
+
+      {
+        type: "Teacher",
+        text: "teacher",
+      },
+
+      {
+        type: "Subject",
+        text: "social studies",
+      },
+
+      {
+        type: "Subject",
+        text: "math",
+      },
+
+      {
+        type: "Subject",
+        text: "language arts",
+      },
     ],
     assignments: [],
     liveStreams: [],
@@ -24,31 +47,43 @@ class StreamContent extends Component {
   componentDidMount() {
     axios.get("/assignments.json").then((response) => {
       let assignments = response.data;
+
       if (!assignments) {
         return;
       } else {
         let updatedAssignments = Object.values(assignments);
-        this.setState({ assignments: updatedAssignments });
+
+        this.setState({
+          assignments: updatedAssignments,
+        });
       }
     });
 
     axios.get("/announcements.json").then((response) => {
       let announcements = response.data;
+
       if (!announcements) {
         return;
       } else {
         let updatedAnnouncements = Object.values(announcements);
-        this.setState({ announcements: updatedAnnouncements });
+
+        this.setState({
+          announcements: updatedAnnouncements,
+        });
       }
     });
 
     axios.get("/liveStreams.json").then((response) => {
       let liveStreams = response.data;
+
       if (!liveStreams) {
         return;
       } else {
         let updatedLiveStreams = Object.values(liveStreams);
-        this.setState({ liveStreams: updatedLiveStreams });
+
+        this.setState({
+          liveStreams: updatedLiveStreams,
+        });
       }
     });
   }
@@ -58,8 +93,7 @@ class StreamContent extends Component {
 
     for (let i = 0; i < this.state.assignments.length; i++) {
       posts.push(
-        <StreamPost
-          // key={}
+        <StreamPost // key={}
           postType={this.state.assignments[i].postType}
           title={this.state.assignments[i].title}
           body={this.state.assignments[i].body}
@@ -70,8 +104,7 @@ class StreamContent extends Component {
 
     for (let i = 0; i < this.state.announcements.length; i++) {
       posts.push(
-        <StreamPost
-          // key={}
+        <StreamPost // key={}
           postType={this.state.announcements[i].postType}
           title={this.state.announcements[i].title}
           body={this.state.announcements[i].body}
@@ -82,8 +115,7 @@ class StreamContent extends Component {
 
     for (let i = 0; i < this.state.liveStreams.length; i++) {
       posts.push(
-        <StreamPost
-          // key={}
+        <StreamPost // key={}
           postType={this.state.liveStreams[i].postType}
           title={this.state.liveStreams[i].title}
           body={this.state.liveStreams[i].body}
@@ -96,16 +128,15 @@ class StreamContent extends Component {
 
     return (
       <div className={classes.StreamContent}>
-        <div>
+        {" "}
+        <div className={classes.Filters}>
+          {" "}
           <Filters
             filters={this.state.filters}
             text={this.state.filters.text}
-          />
-        </div>
-
-        <br />
-
-        <div>{posts}</div>
+          />{" "}
+        </div>{" "}
+        <br /> <div className={classes.Posts}> {posts}</div>{" "}
       </div>
     );
   }
