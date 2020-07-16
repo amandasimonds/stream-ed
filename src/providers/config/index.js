49194a100789b8d99108787e27679c51a6a035fd
Richard Germaine <rlgerma92@outlook.com>
2020-07-16 16:14:58 -0600

    removed .env for now

--- src/providers/config/index.js
+++ src/providers/config/index.js
@@ -4,15 +4,15 @@ import "firebase/database";
 // eslint-disable-next-line
 import dotenv from "dotenv";
 
-export const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
+const firebaseConfig = {
+  apiKey: "AIzaSyA7ofpAsbveL7UsfaObz4x8096MJHKQbPc",
+  authDomain: "stream-ed-2.firebaseapp.com",
+  databaseURL: "https://stream-ed-2.firebaseio.com",
   projectId: "stream-ed-2",
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.RECT_APP_MESSUREMENT_ID,
+  storageBucket: "stream-ed-2.appspot.com",
+  messagingSenderId: "183145365345",
+  appId: "1:183145365345:web:c4252dc015321ec1150f20",
+  measurementId: "G-W4YDFVC31X",
 };
 
 class Firebase {
