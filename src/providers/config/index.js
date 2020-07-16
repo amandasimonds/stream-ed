c8d8685767aa2c2403a11cea1ab463a49cc86a9e
Richard Germaine <rlgerma92@outlook.com>
2020-07-16 14:52:21 -0600

    changed keys to env

--- src/providers/config/index.js
+++ src/providers/config/index.js
@@ -1,16 +1,18 @@
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
+// eslint-disable-next-line
+import dotenv from "dotenv";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA7ofpAsbveL7UsfaObz4x8096MJHKQbPc",
-  authDomain: "stream-ed-2.firebaseapp.com",
-  databaseURL: "https://stream-ed-2.firebaseio.com",
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: "stream-ed-2",
-  storageBucket: "stream-ed-2.appspot.com",
-  messagingSenderId: "183145365345",
-  appId: "1:183145365345:web:c4252dc015321ec1150f20",
-  measurementId: "G-W4YDFVC31X",
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.RECT_APP_MESSUREMENT_ID,
 };
 
 class Firebase {
